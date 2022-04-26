dc707af4b4871efd11537fb60eb2c20ffb8bf328
Jeremy Grecu <jeremy.grecu@sap.com>
2022-04-26 10:33:36 +0100

    JBA - Recipes - Stage 5/5: More chefs to the table -- Configure minimal Spring Security

---
+++ task/src/recipes/security/WebSecurityConfiguration.java
@@ -0,0 +1,43 @@
+package recipes.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/actuator/shutdown").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/register").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("jeremy")
+                .password(getEncoder().encode("password"))
+                .roles()
+                .and()
+                .passwordEncoder(getEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder getEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+}
